47e0da9fb68b5d9b4b226beb196e4586809f4109
cod2048 <cod2048@gmail.com>
2023-07-16 17:47:30 +0900

    script 작성하는 칸 ui 완성

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import logo from './img/logo.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Header from './component/Header';
+import Script from './component/Script';
 
 function App() {
   return (
     <div>
       <Header/>
+      <hr/>
+      <Script/>
       <hr/>
     </div>
   );
