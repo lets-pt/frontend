163c5d54290bde4d6189ee5a96fc320f754f4816
cod2048 <cod2048@gmail.com>
2023-07-19 17:24:17 +0900

    타이머 기능 test페이지에서 연습중(미완성)

--- src/page/TestPage.js
+++ src/page/TestPage.js
@@ -1,15 +1,59 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 
-const MyComponent = () => {
+const Timer = () => {
+  const [selectedTime, setSelectedTime] = useState('');
+  const [remainingTime, setRemainingTime] = useState(0);
+  const [isTimerRunning, setIsTimerRunning] = useState(false);
+
+  const handleTimeChange = (event) => {
+    setSelectedTime(event.target.value);
+  };
+
+  const handleStartTimer = () => {
+    const minutes = parseInt(selectedTime);
+    setRemainingTime(minutes * 60);
+    setIsTimerRunning(true);
+  };
+
+  useEffect(() => {
+    let timerInterval;
+
+    if (isTimerRunning) {
+      timerInterval = setInterval(() => {
+        setRemainingTime((prevTime) => prevTime - 1);
+      }, 1000);
+    }
+
+    return () => {
+      clearInterval(timerInterval);
+    };
+  }, [isTimerRunning]);
+
+  useEffect(() => {
+    if (remainingTime === 0) {
+      setIsTimerRunning(false);
+    }
+  }, [remainingTime]);
+
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+  };
+
   return (
     <div>
-      <h1>홈 페이지</h1>
-      <Link to="/">
-        <button>다른 페이지로 이동</button>
-      </Link>
+      <input
+        type="number"
+        min="1"
+        placeholder="Enter minutes"
+        value={selectedTime}
+        onChange={handleTimeChange}
+      />
+      <button onClick={handleStartTimer}>Start Timer</button>
+      {isTimerRunning && <p>Remaining Time: {formatTime(remainingTime)}</p>}
     </div>
   );
 };
 
-export default MyComponent;
+export default Timer;
